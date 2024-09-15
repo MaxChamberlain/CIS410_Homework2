a6fa2e7fd2f617207a577c7f435efb2c4dbd5a47
MaxChamberlain <maxintegllc@gmail.com>
2024-09-14 19:02:15 -0700

    Move calculations to their own module

---
+++ conversions.py
@@ -0,0 +1,7 @@
+def fahrenheit_to_celsius(f):
+    """Convert Fahrenheit to Celsius."""
+    return round((f - 32) * 5 / 9)
+
+def celsius_to_fahrenheit(c):
+    """Convert Celsius to Fahrenheit."""
+    return round((c * 9 / 5) + 32)
--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def fahrenheit_to_celsius(f):
-    """Convert Fahrenheit to Celsius."""
-    return round((f - 32) * 5 / 9)
-
-def celsius_to_fahrenheit(c):
-    """Convert Celsius to Fahrenheit."""
-    return round((c * 9 / 5) + 32)
+import conversions
     
 def display_title():
     """Display the program title."""
